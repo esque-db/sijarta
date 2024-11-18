a5844d3d0a72ec305fb0c0ac67912fa78399b420
FrankyRay <frankyraymarcellsinaga@gmail.com>
2024-11-18 19:25:15 +0700

    Trigger & Stored Procedure merah

--- sqls/TK2_A_esque_Trigger_4.sql
+++ sqls/TK2_A_esque_Trigger_4.sql
@@ -1,38 +1,47 @@
--- NOT UPDATED
+-- Store Procedure
+CREATE OR REPLACE FUNCTION UPDATE_MYPAY_PEKERJA() RETURN TRIGGER AS
+$$
+    -- Deklarasi variable tersebut untuk transfer saldo MyPay
+    DECLARE
+        IdPekerja UUID;
+        IdKategori UUID;
+        BiayaJasa DECIMAL;
 
-CREATE OR REPLACE FUNCTION VALIDATE_PHONE_NUMBER()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS(
-        SELECT 1
-        FROM "USER"
-        WHERE NoHP = NEW.NoHP
-    ) THEN 
-        RAISE EXCEPTION 'Nomor telepon yang sama sudah terdaftar';
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+    BEGIN
+        -- Jalankan jika status pekerjaan selesai
+        IF NEW.Status = 'Pekerjaan Selesai' THEN
 
-CREATE OR REPLACE TRIGGER TRG_VALIDATE_PHONE_NUMBER
-BEFORE INSERT ON "USER"
-    FOR EACH ROW EXECUTE FUNCTION VALIDATE_PHONE_NUMBER();
+            -- Cari dan simpan biaya jasa dan ID pekerja
+            SELECT PJ.TotalBiaya, PJ.IdPekerja
+                INTO BiayaJasa, IdPekerja
+            FROM STATUS_PESANAN S
+                LEFT JOIN TR_PEMESANAN_STATUS PS ON S.Id = PS.IdStatus
+                LEFT JOIN TR_PEMESANAN_JASA PJ ON PS.IdTrPemesanan = PJ.Id
+            WHERE S.Id = NEW.Id;
 
-CREATE OR REPLACE FUNCTION VALIDATE_UNIQUE_BANK_EMPLOYEE()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS(
-        SELECT 1
-        FROM PEKERJA
-        WHERE NamaBank = NEW.NamaBank
-            AND NomorRekening = NEW.NomorRekening
-    ) THEN 
-        RAISE EXCEPTION 'Kombinasi nama bank dan nomor rekening yang sama sudah terdaftar';
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+            -- Simpan kategori transfer MyPay untuk pembayaran jasa
+            SELECT Id
+                INTO IdKategori
+            FROM KATEGORI_TR_MYPAY
+            WHERE Nama = 'Pembayaran Jasa';
 
-CREATE OR REPLACE TRIGGER TRG_VALIDATE_UNIQUE_BANK_EMPLOYEE
-AFTER UPDATE ON PEKERJA
-    FOR EACH ROW EXECUTE FUNCTION VALIDATE_UNIQUE_BANK_EMPLOYEE();
+            -- Masukkan histori transaksi MyPay
+            INSERT INTO TR_MYPAY (Id, UserId, Tgl, Nominal, KategoriId) VALUES
+            (gen_random_uuid(), IdPekerja, CURRENT_DATE, BiayaJasa, IdKategori);
+
+            -- Tambah saldo pekerja dengan biaya jasa
+            UPDATE USER
+            SET SaldoMyPay = SaldoMyPay + BiayaJasa
+            WHERE Id = IdPekerja;
+        END IF;
+
+        RETURN NEW;
+    END;
+$$
+LANGUAGE plpgsql
+
+-- Trigger
+CREATE OR REPLACE TRIGGER TGR_STATUS_DONE
+AFTER UPDATE ON STATUS_PESANAN
+FOR EACH ROW
+EXECUTE FUNCTION UPDATE_MYPAY();
