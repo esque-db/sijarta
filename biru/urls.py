2dff4469be33a4f831e30a6ec998279701d27f28
allan.kwek <allan.kwek.18@gmail.com>
2024-11-25 18:15:07 +0700

    update navbar

--- biru/urls.py
+++ biru/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import show_testimoni, add_testimoni, show_vouchers, beli_voucher
 
+app_name = 'biru'
+
 urlpatterns = [
     # Testimoni
     path('testimoni/', show_testimoni, name='testimoni_list'),
